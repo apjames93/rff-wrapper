8ce2ab055fb0b97f88eef4e8e1cdad90b7744719
alex james <apjames93@gmail.com>
2020-06-07 08:21:00 -0600

    change script name to deploy-style-guide

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -11,23 +11,13 @@ provider "aws" {
   region                  = "us-west-2"
 }
 
-#backend config on aws s3
-terraform {
-  backend "s3" {
-    bucket = "rff-wrapper-state"
-    key    = "terraformstate"
-    region = "us-west-2"
-  }
-}
-
-
 ################################################################################
 ############################### S3 DOCS #######################################
 ################################################################################
 
 module "s3" {
   source         = "./modules/s3"
-  bucket         = "rff-wrapper-docs.com"
+  bucket         = "rff-wrapper-style-guide.com"
   acl            = "public-read"
   index_document = "index.html"
   error_document = "index.html"
