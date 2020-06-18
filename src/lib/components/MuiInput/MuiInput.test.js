bf7943d28706ec9a6e796cf3c5756f6d1d3715e0
Isaac Sunoo <isaacsunoo@gmail.com>
2020-06-18 17:00:13 -0600

    removed sinon

--- src/lib/components/MuiInput/MuiInput.test.js
+++ src/lib/components/MuiInput/MuiInput.test.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 import React from 'react';
 import { mount } from 'enzyme';
-import sinon from 'sinon';
 import renderer from 'react-test-renderer';
 import { MuiInput, customPropName } from './MuiInput';
 import Form from '../Form/Form';
@@ -18,20 +17,7 @@ function setup(name = undefined) {
   return { comp, props };
 }
 
-function expectMissingProp(prop, component) {
-  console.log(prop);
-  sinon.assert.calledWithMatch(console.error,
-    new RegExp(`The prop \`${prop}\` is marked as required in \`${component}\`, but its value is \`undefined\`.`));
-}
-
 describe('<MuiInput />', () => {
-  beforeEach(() => {
-    sinon.stub(console, 'error');
-  });
-
-  afterEach(() => {
-    console.error.restore();
-  });
   it('renders MuiInput', () => {
     const { comp } = setup('cool');
     expect(comp).toBeDefined();
@@ -59,7 +45,6 @@ describe('<MuiInput />', () => {
   });
 
   it('should return an error if no fields prop and no names prop is passed down', () => {
-    const { comp } = setup();
     const errorProp = customPropName({ fields: undefined }, 'apples', 'MuiInput');
     expect(errorProp.toString()).toEqual(
       'Error: MuiInput: apples requires to have name value if fields are not passed down. Please wrap this in RFFField Array or add a name prop.',
