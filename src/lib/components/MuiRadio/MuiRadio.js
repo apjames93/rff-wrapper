90ca04a459e71aae29ca8efa925cdc8ad789da3b
alex james <apjames93@gmail.com>
2020-06-25 14:24:50 -0600

    not nesting MUI comps

--- src/lib/components/MuiRadio/MuiRadio.js
+++ src/lib/components/MuiRadio/MuiRadio.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Radio, FormControlLabel } from '@material-ui/core';
-import MuiIcon from '../MUI/MuiIcon/MuiIcon';
+import MuiIcon from '../MuiIcon/MuiIcon';
 import RFFField from '../RFFField/RFFField';
 
 export const MuiRadio = ({
