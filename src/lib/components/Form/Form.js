5f51d49a8b1e8407f1b8edf831351787a5fd00f2
Isaac Sunoo <isaacsunoo@gmail.com>
2020-07-02 12:41:37 -0600

    added mutators to proptypes in Form

--- src/lib/components/Form/Form.js
+++ src/lib/components/Form/Form.js
@@ -54,8 +54,8 @@ export default RffWrapper;
 // https://final-form.org/docs/react-final-form/types/FormProps
 RffWrapper.propTypes = {
   /**
-     * A render function that is given FormRenderProps, as well as any non-API props passed into the <Form/> component.
-    */
+   * A render function that is given FormRenderProps, as well as any non-API props passed into the <Form/> component.
+   */
   children: PropTypes.node.isRequired,
   /**
    * Function to call when the form is submitted. There are three possible ways to write an onSubmit function:
@@ -63,16 +63,16 @@ RffWrapper.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 
   /**
-     * A callback for debugging that receives the form state and the states of all the fields. It's called on every state change. A typical thing to pass in might be console.log.
-     */
+   * A callback for debugging that receives the form state and the states of all the fields. It's called on every state change. A typical thing to pass in might be console.log.
+   */
   debug: PropTypes.func,
   /**
-    * The initial values of your form. These will also be used to compare against the current values to calculate pristine and dirty.
+   * The initial values of your form. These will also be used to compare against the current values to calculate pristine and dirty.
    */
   initialValues: PropTypes.shape(),
   /**
-     * A predicate to determine whether or not the initialValues prop has changed, i.e. to know if the form needs to be reinitialized with the new values. Useful for passing in a "deep equals" function if you need to. Defaults to "shallow equals".
-    */
+   * A predicate to determine whether or not the initialValues prop has changed, i.e. to know if the form needs to be reinitialized with the new values. Useful for passing in a "deep equals" function if you need to. Defaults to "shallow equals".
+   */
   initialValuesEqual: PropTypes.bool,
   /**
    * If true, only pristine values will be overwritten when initialize(newValues) is called. This can be useful for allowing a user to continue to edit a record while the record is being saved asynchronously, and the form is reinitialized to the saved values when the save is successful. Defaults to false.
@@ -93,6 +93,11 @@ RffWrapper.propTypes = {
    * If true, validation will happen on blur. If false, validation will happen on change. Defaults to false.
    */
   validateOnBlur: PropTypes.bool,
+  /**
+   * Optional.
+   * Named mutator functions.
+   */
+  mutators: PropTypes.shape(),
 };
 
 RffWrapper.defaultProps = {
