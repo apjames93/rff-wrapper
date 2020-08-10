e412557e3f9421a8ac8b8263dc04d80baa76e12d
Isaac Sunoo <isaacsunoo@gmail.com>
2020-08-10 14:12:07 -0600

    updated package:

--- src/lib/components/TelephoneInput/TelephoneInput.js
+++ src/lib/components/TelephoneInput/TelephoneInput.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReactTelephoneInput from 'react-telephone-input';
 import RFFField from '../RFFField/RFFField';
-import flagsImagePath from '../../../images/flags.png';
+import flagsImagePath from '../../../images/flags1.png';
 
 const TelephoneInput = ({
   // rff
