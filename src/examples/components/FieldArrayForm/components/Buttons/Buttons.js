337ed8c1b8ee6811546c4dab3c4048e6093cce19
alex james <apjames93@gmail.com>
2020-08-11 12:44:47 -0600

    FieldArrayForm form example

---
+++ src/examples/components/FieldArrayForm/components/Buttons/Buttons.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Buttons = ({
+  form,
+}) => (
+  <div>
+    <button
+      type="button"
+      onClick={() => form.mutators.push('people', undefined)}
+    >
+      Add Person
+    </button>
+    <button type="button" onClick={() => form.mutators.pop('people')}>
+      Remove Person
+    </button>
+  </div>
+);
+
+export default Buttons;
+
+Buttons.propTypes = {
+  // rff wrapper passes from prop to each root child element 
+  form: PropTypes.shape({
+    mutators: PropTypes.shape({
+      push: PropTypes.func,
+      pop: PropTypes.func,
+    }),
+  }).isRequired,
+};
