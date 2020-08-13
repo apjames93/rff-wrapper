c47b203fc7ba2dd29f05329f3501ba38ebe224ed
alex james <apjames93@gmail.com>
2020-08-13 15:36:26 -0600

    useEffect to rerender comp to set TextField value on input change value

--- src/lib/components/MuiSelect/components/Select/Select.js
+++ src/lib/components/MuiSelect/components/Select/Select.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { TextField } from '@material-ui/core';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -39,6 +39,10 @@ export const Select = ({
     }
     return null;
   };
+
+  useEffect(() => {
+    setTextFieldValue('');
+  }, [field.input.value]);
 
   return (
     <TextField
